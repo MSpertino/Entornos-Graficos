da8e58a7b9e340938afee7517004aa269edf98df
Mateo Spertino <122882078+MSpertino@users.noreply.github.com>
2026-01-27 15:57:34 -0300

    wd

--- api/shared/detalle-atencionAP.php
+++ api/shared/detalle-atencionAP.php
@@ -54,7 +54,7 @@ if (!$atencion) {
     <title>Detalle Atención</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="../styles.css" rel="stylesheet">
+    <link href="/styles.css" rel="stylesheet">
 </head>
 
 <body>
